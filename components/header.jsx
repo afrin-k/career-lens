11b70a09b9d075948901f73e48280c7732738bff
afrin-k <89728611+afrin-k@users.noreply.github.com>
2026-03-18 05:19:06 +0530

    updated header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -5,10 +5,8 @@ import React from 'react'
 import { Button } from './ui/button'
 import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
-import { checkUser } from '@/lib/checkUser'
 
-const Header = async () => {
-   await checkUser();
+const Header = () => {
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-background">
         <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
