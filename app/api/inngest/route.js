69e3ecc169f6fd0f5bcf966a77266261580ff0d6
afrin-k <89728611+afrin-k@users.noreply.github.com>
2026-03-18 05:36:56 +0530

    updated app/api/inngest/route.js
    Updated API to serve functions with runtime specified.

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -4,11 +4,12 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/functions";
 
-//creating an api that serves zero functions
+export const runtime = "nodejs";
+
+//creating an api that serves functions
 export const { GET, POST, PUT } = serve({
-    client: inngest,
-    functions: [
-        /* your functions will be passed here later */
-        generateIndustryInsights,
-    ],
+  client: inngest,
+  functions: [
+    generateIndustryInsights,
+  ],
 });
