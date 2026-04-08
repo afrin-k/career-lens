b4fd25cfd35b69c7b6e0c14447bb24dee2b515a6
Afrin Khanam <afrink@Afrins-MacBook-Air.local>
2026-04-08 05:53:43 +0530

    new package

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import React from 'react'
 
-const NotFoundPage = () => {
+const NotFoundPage = async() => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen px-4 text-center'>
         <h1 className='text-6xl font-bold gradient-title'>404</h1>
